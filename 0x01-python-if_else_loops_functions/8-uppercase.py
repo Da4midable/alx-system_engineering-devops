db993689fb53bc71f36d1f9edc257c862e43726e
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-10-31 12:42:25 -0700

    8. To uppercase - Update 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(s):
     for char in s:
         if ord('a') <= ord(char) <= ord('z'):
             print("{}".format(chr(ord(char) - 32)), end='')
         else:
             print("{}".format(char), end='')
-
     print()
