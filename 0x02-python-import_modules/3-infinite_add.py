7ee7e92ca3627b5b2cceed5f8f8b0070ab4170b6
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-02 09:34:27 -0700

    Python - 3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    res_add = 0
+    for i in range(1, len(sys.argv)):
+        res_add += int(sys.argv[i])
+        
+    print("{:d}".format(res_add))
