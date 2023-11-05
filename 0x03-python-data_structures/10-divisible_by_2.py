0cdfe50cc2a31a946cf7931023e6544c7b252a6a
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-05 10:06:07 -0800

    Python 10. Only by 2 - Update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     results = []
     for num in my_list:
-        div_2 =  num % 2 == 0
+        div_2 = num % 2 == 0
         if div_2:
             results.append(True)
         else:
