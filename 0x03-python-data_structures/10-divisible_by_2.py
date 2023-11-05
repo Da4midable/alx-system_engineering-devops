c468415a9be6a8ed8a5b56e2b049f0a0d5b0ec51
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-05 10:05:05 -0800

    Python - 10. Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    results = []
+    for num in my_list:
+        div_2 =  num % 2 == 0
+        if div_2:
+            results.append(True)
+        else:
+            results.append(False)
+    return results
