39476ba15b9a8d6a170a5fb24907225afe578b24
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-06 09:01:06 -0800

    Python - 11. Delete at - Update 2

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 and idx > len(my_list):
+    if my_list is None:
+        my_list = []
+    if idx < 0 and idx >= len(my_list):
         return my_list
     else:
         if my_list:
