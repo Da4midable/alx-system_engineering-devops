4edd65bf7ccb8d92b0db9f23c7b58524b70ed127
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-20 11:40:06 +0100

    slight modifications

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range((0), (9) + 1):
+for x in range((0), (9)):
     for y in range(x + 1, 10):
         if x != 8 or y != 9:
             print("{}".format(x) + "" + "{}".format(y), end=", ")
