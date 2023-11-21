b06112cfdfa2351410e4c07439f3f385ee36c230
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 15:37:17 +0100

    empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A module for square"""
+"""A module for printing square"""
 
 
 class Square:
-    """Represent a square."""
+    """Square class with a method to print a square."""
     pass
