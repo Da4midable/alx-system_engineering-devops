97423d9932185480e878611860b432bbd11c8994
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 19:43:29 +0100

    Square that defines a square based on 3-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Square class with a method to print a square.
+
+    Attributes:
+        __size (int): The length of the side of the square.
+
+    Methods:
+        __init__(self, size=0): Constructor to initialize the Square instance.
+    """
+
+    def __init__(self, size=0):
+        """Constructor to initialize a new square.
+
+        Args:
+            size (int): Length of new square.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method."""
+        if not isinstance(value, int):
+            raise TypeError("Size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of size (int)"""
+        return self.__size * self.__size
