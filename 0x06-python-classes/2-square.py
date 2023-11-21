4850d8d5c68ebb782e4035cab9d06fe5d02617e1
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 17:30:46 +0100

    Square class with a method to print a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Square class with a method to print a square.
+
+    Attributes:
+        __size (int): The length of the side of the square.
+
+    Methods:
+        __init__(self, size=0): Constructor to initialize the Square instance.
+    """
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size (int): Length of side of the square.
+
+        Raises:
+            ValueError: If size is less than 0.
+            TypeError: If size is not an integer.
+        """
+        self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
