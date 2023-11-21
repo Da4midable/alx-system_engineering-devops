8ecec071b429481015b13c60eb57deb8b0a95cae
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 15:30:36 +0100

    0.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-class Square:
-    """Square class with a method to print a square."""
+"""A module for square"""
 
-    def print_square(self):
-        """Prints a square using the print statement."""
-        print("{}")
+
+class Square:
+    """Represent a square."""
+    pass
