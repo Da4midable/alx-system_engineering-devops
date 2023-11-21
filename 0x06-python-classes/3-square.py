d4b38354d58b7acce511c9365d57a36ed554fcb2
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 18:20:09 +0100

    Square that defines a square by: 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -27,3 +27,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Returns the area of size (int)"""
+        return self.__size * self.__size
