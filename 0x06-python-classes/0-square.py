c1f4b07c24fceed75d1ad3403a5a740696008227
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-21 15:22:12 +0100

    empty class Square that defines a square (+doc)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """Square class with a method to print a square."""
+
     def print_square(self):
+        """Prints a square using the print statement."""
         print("{}")
