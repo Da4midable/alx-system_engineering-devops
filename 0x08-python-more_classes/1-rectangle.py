4d726304647f33010d587e6f6af0e134aa7cc66e
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-27 15:24:21 +0100

    retrieve and set width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Rectangle:
+    """Rectangle class with a method to print a square."""
+    def __init__(self, width=0, height=0):
+        """Constructor.
+        Args:
+            width: width of the rectangle.
+            height: height of rectangle.
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Getter method."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Setter method."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Setter method."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
