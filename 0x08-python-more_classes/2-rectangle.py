188902d6069d975f34aa7c44043ddc61d94e04d8
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-27 15:41:13 +0100

    defines area and perimeter of rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Rectangle:
+    """Rectangle class with a method to print a square."""
+    def __init__(self, width=0, height=0):
+        """Constructor.
+        Args:
+            width: width of the rectangle.
+            height: height of rectangle.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Getter method."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Setter method."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Setter method."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ returns the area of a rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """ returns the area of a rectangle"""
+        result = self.__height + self.__width
+        return (2 * result)
