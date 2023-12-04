584dd0ea1b3a5b6d38864555db5890e8490f3283
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-04 14:37:30 +0100

    Check if object type is same as specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module: my_module
+~~~~~~~~~~~~~~~~~
+
+A module containing a function to check if an object is of a specific class.
+
+Functions:
+- is_same_class(obj, a_class): Check if object type is same as specified class
+
+Usage:
+>>> from my_module import is_same_class
+
+>>> obj_instance = MyClass()
+>>> is_same_class(obj_instance, MyClass)
+True
+
+>>> other_instance = OtherClass()
+>>> is_same_class(other_instance, MyClass)
+False
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    Check if the type of an object is the same as a specified class.
+
+    Parameters:
+    - obj: Any Python object to check.
+    - a_class: class to compare the object type with
+
+    Returns:
+    True if object type is the same as the specified class, False otherwise.
+
+    Example:
+    >>> from my_module import is_same_class
+
+    >>> obj_instance = MyClass()
+    >>> is_same_class(obj_instance, MyClass)
+    True
+
+    >>> other_instance = OtherClass()
+    >>> is_same_class(other_instance, MyClass)
+    False
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
