2e887175c1031e60255733e10a53da7bd22eafb1
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-04 15:21:49 +0100

    empty class for geometry-related functionality

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+
+A module containing an empty base class for geometry-related functionality.
+
+Classes:
+- BaseGeometry: An empty base class for geometry-related functionality.
+
+Usage:
+>>> from my_module import BaseGeometry
+
+>>> geometry_instance = BaseGeometry()
+"""
+
+
+class BaseGeometry:
+    """
+    An empty base class for geometry-related functionality.
+
+    This class serves as a foundation for building geometry-related classes.
+    It doesn't implement any specific functionality but can be used as a
+    base class for deriving more specialized geometry classes.
+
+    Usage:
+    >>> from my_module import BaseGeometry
+
+    >>> geometry_instance = BaseGeometry()
+    """
+    pass
