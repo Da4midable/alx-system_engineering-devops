3504625f87d3c5cf1ce943bf54f3538f6518d768
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-04 19:17:34 +0100

    a class representing a square with equal sides+

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,7 @@
 """
 
 A module containing the `Square` class, which inherits
-from the `Rectangle` class.
+the `Rectangle` class.
 
 Classes:
 - Square: A class representing a square with equal sides.
@@ -14,7 +14,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """
     Square class representing a square with equal sides,
-    inheriting from the `Rectangle` class.
+    inheriting the `Rectangle` class.
 
     Attributes:
     - __size (int): The size of the square.
@@ -23,10 +23,6 @@ class Square(Rectangle):
     - __init__(self, size): Constructor method to initialize a Square object
     with a given size.
 
-    Usage:
-    >>> from my_module import Square
-
-    >>> square_instance = Square(4)
     """
 
     def __init__(self, size):
@@ -40,10 +36,6 @@ class Square(Rectangle):
         - TypeError: If size is not an integer.
         - ValueError: If size is less than or equal to 0.
 
-        Usage:
-        >>> from my_module import Square
-
-        >>> square_instance = Square(4)
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
