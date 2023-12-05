1fabc5d143ada6803b2929a50c7ced25c3de66f1
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-05 22:50:48 +0100

    converts a class instance to a JSON representation

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+def class_to_json(obj):
+    """
+    Convert a class instance to a JSON representation.
+
+    Parameters:
+    - obj: The instance of a class to be converted.
+
+    Returns:
+    - dict: A dictionary representing the attributes of the class instance.
+
+    Example:
+    >>> class MyClass:
+    ...     def __init__(self, name, age):
+    ...         self.name = name
+    ...         self.age = age
+    ...
+    >>> obj = MyClass("John", 25)
+    >>> json_representation = class_to_json(obj)
+    >>> print(json_representation)
+    {'name': 'John', 'age': 25}
+    """
+    return obj.__dict__
