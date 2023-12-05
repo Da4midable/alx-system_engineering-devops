17c280fad93931840c45c9a0760dfb98762a2c83
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-05 15:17:24 +0100

    returns JSON representation of a Python object+

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,25 +1,43 @@
+#!/usr/bin/python3
+"""
+Module: my_module
+~~~~~~~~~~~~~~~~~
+
+Module providing a function to convert a Python object to JSON representation
+
+Functions:
+- to_json_string(my_obj): Return the JSON representation of a Python object.
+
+"""
+
 import json
 
 
 def to_json_string(my_obj):
     """
-    Return the JSON representation of an object (string).
+    Return the JSON representation of a Python object.
 
-    Parameters:
+    This function uses `json.dumps` method to serialize given Python object
+    to its JSON representation.
+
+    Args:
     - my_obj: The Python object to be serialized.
 
     Returns:
     - str: The JSON representation of the object.
 
     Example:
-    >>> to_json_string([1, 2, 3])
-    '[1, 2, 3]'
+    >>> from my_module import to_json_string
 
-    >>> to_json_string({'key': 'value'})
-    '{"key": "value"}'
+    >>> my_list = [1, 2, 3]
+    >>> json_list = to_json_string(my_list)
+    >>> print(json_list)
+    "[1, 2, 3]"
 
-    >>> to_json_string(42)
-    '42'
+    >>> my_dict = {'key': 'value', 'number': 42}
+    >>> json_dict = to_json_string(my_dict)
+    >>> print(json_dict)
+    '{"key": "value", "number": 42}'
 
     """
     json_rep = json.dumps(my_obj)
