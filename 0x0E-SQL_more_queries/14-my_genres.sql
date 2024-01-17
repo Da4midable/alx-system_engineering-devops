13bbb2739762b7d8aca5c49bbb8ce65ff7887c71
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-01-17 21:39:01 +0100

    Task 14+

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,9 @@
 --  uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
-FROM tv_genres RIGHT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC
+USE hbtn_0d_tvshows;
+
+SELECT tv_genres.name 
+FROM tv_show_genres, tv_genres, tv_shows 
+WHERE tv_shows.title = 'Dexter' 
+AND tv_shows.id = tv_show_genres.tv_show_id 
+AND tv_genres.id = tv_show_genres.genre_id 
+ORDER BY tv_genres.name ASC
