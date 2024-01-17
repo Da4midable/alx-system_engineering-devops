1594ea5a659d63531f721ade4c7b9cd9c14d927e
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-01-17 17:23:26 +0100

    Task 6+

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on my MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT DEFAULT UNIQUE NOT NULL AUTO_INCREMENT, VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS states (
+  id INT NOT NULL UTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
