b79571411410f115890069be1a32ab1dc994551c
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-02-12 06:51:38 -0800

    print x's

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    let line = '';
+    for (let j = 0; j < num; j++) {
+      line += 'x';
+    }
+    console.log(line);
+  }
+}
