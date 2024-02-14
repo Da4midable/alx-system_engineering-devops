141222745ae93f7c0f5ea7fdd153601c871abe3a
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-02-13 23:59:57 -0800

    102 fixed

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const file1 = fs.readFileSync('fileA', 'utf8');
 const file2 = fs.readFileSync('fileB', 'utf8');
 
-const combined = file1 + '\n' + file2 + '\n';
+const combined = file1 + file2;
 
 fs.writeFileSync('fileC', combined);
